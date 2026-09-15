189e0fd93357b8ce90b46e7d3f65ce9a8e92ae14
Emrah Abdurahman <emrah.abdurahman@hotmail.com>
2026-09-15 13:20:44 +0100

    feat: implement Explorer-style folder rendering with double-click navigation

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,24 @@
+'use client'
+
+import { useState } from 'react'
+
 import ApplicationContainer from '@/components/ui/application-container'
 import ContentPanel from '@/components/ui/content-panel'
 import NavigationPanel from '@/components/ui/navigation-panel'
+import { MOCK_FOLDERS } from '@/data/mock-data'
 
 export default function Home() {
+  const [currentFolderId, setCurrentFolderId] = useState<number | null>(null)
+
+  const updateCurrentFolderId = (id: number | null) => {
+    setCurrentFolderId(id)
+  }
+
+  const folders = MOCK_FOLDERS.filter((folder) => folder.parentFolderId === currentFolderId)
+
   return (
     <ApplicationContainer>
-      <NavigationPanel />
+      <NavigationPanel currentFolderId={currentFolderId} updateCurrentFolderId={updateCurrentFolderId} folders={folders} />
       <ContentPanel />
     </ApplicationContainer>
   )
--- src/components/ui/navigation-panel.tsx
+++ src/components/ui/navigation-panel.tsx
@@ -1,5 +1,27 @@
+import { Folder } from '@/models/folder'
+
 import styles from './navigation-panel.module.css'
 
-export default function NavigationPanel() {
-  return <section className={styles.navigationPanel}>Navigation Section</section>
+export interface NavigationPanelProps {
+  currentFolderId: number | null
+  updateCurrentFolderId: (id: number | null) => void
+  folders: Folder[]
+}
+
+export default function NavigationPanel({ currentFolderId, updateCurrentFolderId, folders }: NavigationPanelProps) {
+  return (<section className={styles.navigationPanel}>
+    <p>Navigation Panel</p>
+    {folders.map(folder => {
+      return (
+        <button
+          key={folder.id}
+          className={styles.navigationFolder}
+          type='button'
+          onDoubleClick={() => { updateCurrentFolderId(folder.id) }}>
+          {folder.name}
+        </button>
+      )
+    })}
+  </section>
+  )
 }
